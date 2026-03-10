5d6bbc54e0b1b05b5241803d595ef13d6d33b6cd
manuc <manuc@Manuel>
2026-03-10 18:15:03 +0100

    Initial Spring Boot project

---
+++ src/test/java/com/mgcss/serviceplatform/MgcssServicePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgcss.serviceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgcssServicePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
